ed765239e5ea0fc052c0ff841cc46cf95c037a02
gordon8 <973540381@qq.com>
2018-01-22 22:57:46 +0800

    header

---
+++ src/components/header/header.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="header">
+      i am a header
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "header"
+    }
+</script>
+
+<style scoped>
+
+</style>
