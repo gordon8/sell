2c6367dc69debf1047bce772f5ce709549b9c709
gordon <973540381@qq.com>
2018-03-13 07:22:51 +0800

    star

---
+++ src/components/star/star.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="star">
+    <span v-for="item in itemClass" :class="['star-item', item]"></span>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      size: {
+        type: Number
+      },
+      score: {
+        type: Number
+      }
+    },
+    computed: {
+      starType() {
+        return 'star-' + this.size;
+      },
+      itemClass() {
+        let result = [];
+        let
+
+        return result;
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
