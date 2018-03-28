bb644ac42404ef56770b5dcdfa7b57fef55d65f5
gordon8 <973540381@qq.com>
2018-03-28 17:49:56 +0800

    ratinglist

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -33,20 +33,21 @@
             <ratingselect :food="selectedFood" :selectedType="selectedType" :onlyContent="onlyContent" :desc="desc" @select="selectType" @toggle="toggleContent"></ratingselect>
           </div>
           <ul class="rating-list">
-            <li v-for="item in selectedFood.ratings" class="rating-item">
-              <p class="time">{{item.rateTime}}</p>
-              <p class="msg">{{item.text}}</p>
+            <li v-for="item in selectedFood.ratings" class="rating-item" :key="item.rateTime" v-show="needShow(item.rateType, item.text)">
+              <p class="time">{{item.rateTime | formatDate}}</p>
+              <p class="msg">
+                <i v-show="item.rateType === 0" class="iconfont icon-dianzan positive"></i>
+                <i v-show="item.rateType === 1" class="iconfont icon-cai negative"></i>
+                <span class="text">{{item.text}}</span>
+              </p>
               <div class="user">
                 <span class="name">{{item.username}}</span>
-                <img :src="item.avatar" alt="头像" class="face">
+                <img :src="item.avatar" alt="头像" width="12" height="12" class="face">
               </div>
             </li>
           </ul>
-
         </div>
       </div>
-
-
     </div>
   </transition>
 </template>
@@ -54,11 +55,12 @@
 <script type="text/ecmascript-6">
   import BScroll from 'better-scroll';
   import Vue from 'vue';
+  import {formatDate} from "../../common/js/date";
   import cartcontrol from '@/components/cartcontrol/cartcontrol';
   import split from '@/components/split/split';
   import ratingselect from '@/components/ratingselect/ratingselect';
 
-  const ALL = 0;
+  const ALL = 2;
 
   export default {
     name: "food",
@@ -80,11 +82,27 @@
       }
     },
     methods: {
+      needShow(type, text) {
+        if (this.onlyContent && !text) {
+          return false;
+        }
+        if (this.selectedType === ALL) {
+          return true;
+        } else if (this.selectedType === type) {
+          return true;
+        }
+      },
       selectType(type) {
         this.selectedType = type;
+        this.$nextTick(() => {
+          this.scroll.refresh();
+        });
       },
       toggleContent() {
         this.onlyContent = !this.onlyContent;
+        this.$nextTick(() => {
+          this.scroll.refresh();
+        });
       },
       addFirst(event) {
         this.$emit('add', event.target);
@@ -95,6 +113,8 @@
       },
       show() {
         this.foodShow = true;
+        this.selectedType = ALL;
+        this.onlyContent = true;
         this.$nextTick(() => {
           if (!this.scroll) {
             this.scroll = new BScroll(this.$refs.food, {
@@ -113,12 +133,19 @@
       cartcontrol,
       split,
       ratingselect
+    },
+    filters: {
+      formatDate(time) {
+        let date = new Date(time);
+        return formatDate(date, 'YYYY-MM-DD hh:mm');
+      }
     }
   }
 </script>
 
 <style scoped lang="scss" rel="stylesheet/scss" type="text/scss">
   @import "../../common/css/iconfont.css";
+  @import "../../common/css/mixin";
   .food-wrap {
     position: fixed;
     top: 0;
@@ -237,6 +264,53 @@
       }
       .rating-list{
         border-top: 1px solid rgba(7,17,27,0.1);
+        .rating-item {
+          position: relative;
+          margin: 0 18px;
+          padding: 16px 0;
+          @include border-1px(rgba(7,17,27,0.1));
+          .time {
+            font-size: 10px;
+            color: rgb(147,153,159);
+          }
+          .msg {
+            margin-top: 6px;
+            font-size: 0;
+            .iconfont {
+              display: inline-block;
+              font-size: 12px;
+              margin-right: 4px;
+              &.positive {
+                color: rgb(0,160,220);
+              }
+              &.negative {
+                color: rgb(147,153,159);
+              }
+            }
+            .text {
+              display: inline-block;
+              font-size: 12px;
+              color: rgb(7,17,27);
+            }
+          }
+          .user {
+            position: absolute;
+            right: 0;
+            top: 16px;
+            font-size: 0;
+            .name {
+              display: inline-block;
+              margin-right: 6px;
+              font-size: 10px;
+              line-height: 12px;
+              color: rgb(147,153,159);
+            }
+            .face {
+              vertical-align: top;
+              border-radius: 50%;
+            }
+          }
+        }
       }
     }
 
